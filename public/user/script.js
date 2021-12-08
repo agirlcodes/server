23724978734767fa7a0bc83361f27e192018efd5
Nataly Angulo <nataly.angulo530@gmail.com>
2021-12-08 05:40:41 +0000

    ip to local

--- public/user/script.js
+++ public/user/script.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       // SAVE INPUT TO DATABASE
       function save() {
-        fetch('http://68.183.39.213/userdata', {
+        fetch('http://localhost:3000/userdata', {
           method: 'POST',
           headers: {
             'Accept':'application/json',
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     // GETTING DATA FROM DATABASE 
       function load() {
-        fetch('http://68.183.39.213/userdata')
+        fetch('http://localhost:3000/userdata')
           .then(res => res.json())
           .then(data => {
             let dataArray = data;
@@ -118,7 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
                               // if (todoList.id == userTaskid);
                               done = !done
                               console.log(done)
-                              fetch('http://68.183.39.213/userdata/', {
+                              fetch('http://localhost:3000/userdata/', {
                                 method: 'PUT',
                                 headers: {
                                   'Accept':'application/json',
@@ -131,7 +131,7 @@ document.addEventListener('DOMContentLoaded', () => {
                               save()
                             }
                       function deleteItem(){
-                          fetch('http://68.183.39.213/userdata', {
+                          fetch('http://localhost:3000/userdata', {
                             headers: {
                               'Accept':'application/json',
                               'Content-Type':'application/json'
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', () => {
       //     function deleteItem(e){
       //       // function dataDelete(){
       //         e.preventDefault();
-      //         fetch('http://68.183.39.213/userdata', {
+      //         fetch('http://localhost:3000/userdata', {
       //           headers: {
       //             'Accept':'application/json',
       //             'Content-Type':'application/json'
@@ -184,7 +184,7 @@ document.addEventListener('DOMContentLoaded', () => {
       //       tr.classList.toggle("strike"); 
       //       if (todoList.id == this.dataset.id);
       //       // done = !done
-      //       fetch('http://68.183.39.213/userdata/${userdata.done}', {
+      //       fetch('http://localhost:3000/userdata/${userdata.done}', {
       //         method: 'PUT',
       //         headers: {
       //           'Accept':'application/json',
@@ -252,7 +252,7 @@ document.addEventListener('DOMContentLoaded', () => {
       else{
         // ADDING TASK TO DATABASE
         console.log("adding to database")
-        fetch('http://68.183.39.213/tasks', {
+        fetch('http://localhost:3000/tasks', {
             method: 'POST',
             headers: {
               'Accept':'application/json',
@@ -267,7 +267,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
    // DELETE TASKS
   // add task to option
-  fetch('http://68.183.39.213/tasks')
+  fetch('http://localhost:3000/tasks')
   .then(response =>response.json())
   .then(data =>{ 
     let editData = document.getElementById('editTaskList')
@@ -281,7 +281,7 @@ document.addEventListener('DOMContentLoaded', () => {
       deleteBtn.addEventListener('click', () => {
         let taskId = document.getElementById("editTaskList").value
         console.log(taskId)
-            fetch('http://68.183.39.213/tasks/', {
+            fetch('http://localhost:3000/tasks/', {
               headers: {
                 'Content-type': 'application/json'
               },
@@ -294,7 +294,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   
   //API FOR TO-DO LIST
-  fetch('http://68.183.39.213/tasks')
+  fetch('http://localhost:3000/tasks')
   .then(response =>response.json())
   .then(response =>{ 
     let data = response
